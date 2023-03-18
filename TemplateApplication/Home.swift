8d6e3ef38f65e51057bee22b6e8e63c93a7d45e1
Oliver Aalami <aalami@gmail.com>
2023-03-18 15:00:30 -0600

    Initial commit

---
+++ TemplateApplication/Home.swift
@@ -0,0 +1,57 @@
+//
+// This source file is part of the Stanford CardinalKit Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import FHIRMockDataStorageProvider
+import SwiftUI
+import TemplateContacts
+import TemplateSchedule
+import TemplateSharedContext
+
+
+struct HomeView: View {
+    enum Tabs: String {
+        case schedule
+        case contact
+        case mockUpload
+    }
+    
+    
+    @AppStorage(StorageKeys.homeTabSelection) var selectedTab = Tabs.schedule
+    
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            ScheduleView()
+                .tag(Tabs.schedule)
+                .tabItem {
+                    Label("SCHEDULE_TAB_TITLE", systemImage: "list.clipboard")
+                }
+            Contacts()
+                .tag(Tabs.contact)
+                .tabItem {
+                    Label("CONTACTS_TAB_TITLE", systemImage: "person.fill")
+                }
+            MockUploadList()
+                .tag(Tabs.mockUpload)
+                .tabItem {
+                    Label("MOCK_UPLOAD_TAB_TITLE", systemImage: "server.rack")
+                }
+        }
+    }
+}
+
+
+#if DEBUG
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+            .environmentObject(TemplateApplicationScheduler())
+            .environmentObject(MockDataStorageProvider())
+    }
+}
+#endif
---
+++ TemplateApplication/TemplateAppDelegate.swift
@@ -0,0 +1,77 @@
+//
+// This source file is part of the Stanford CardinalKit Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import CardinalKit
+import FHIR
+import FHIRMockDataStorageProvider
+import FHIRToFirestoreAdapter
+import FirebaseAccount
+import class FirebaseFirestore.FirestoreSettings
+import FirebaseAuth
+import FirestoreDataStorage
+import FirestoreStoragePrefixUserIdAdapter
+import HealthKit
+import HealthKitDataSource
+import HealthKitToFHIRAdapter
+import Questionnaires
+import Scheduler
+import SwiftUI
+import TemplateSchedule
+import TemplateSharedContext
+
+
+class TemplateAppDelegate: CardinalKitAppDelegate {
+    override var configuration: Configuration {
+        Configuration(standard: FHIR()) {
+            if !FeatureFlags.disableFirebase {
+                if FeatureFlags.useFirebaseEmulator {
+                    FirebaseAccountConfiguration(emulatorSettings: (host: "localhost", port: 9099))
+                } else {
+                    FirebaseAccountConfiguration()
+                }
+                firestore
+            }
+            if HKHealthStore.isHealthDataAvailable() {
+                healthKit
+            }
+            QuestionnaireDataSource()
+            MockDataStorageProvider()
+            TemplateApplicationScheduler()
+        }
+    }
+    
+    
+    private var firestore: Firestore<FHIR> {
+        let settings = FirestoreSettings()
+        if FeatureFlags.useFirebaseEmulator {
+            settings.host = "localhost:8080"
+            settings.isPersistenceEnabled = false
+            settings.isSSLEnabled = false
+        }
+        
+        return Firestore(
+            adapter: {
+                FHIRToFirestoreAdapter()
+                FirestoreStoragePrefixUserIdAdapter()
+            },
+            settings: settings
+        )
+    }
+    
+    
+    private var healthKit: HealthKit<FHIR> {
+        HealthKit {
+            CollectSample(
+                HKQuantityType(.stepCount),
+                deliverySetting: .anchorQuery(.afterAuthorizationAndApplicationWillLaunch)
+            )
+        } adapter: {
+            HealthKitToFHIRAdapter()
+        }
+    }
+}
