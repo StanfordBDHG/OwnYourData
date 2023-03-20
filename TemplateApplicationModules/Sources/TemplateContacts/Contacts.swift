60777d2d0d39457e07435db646a8a0a973da1e0c
Oliver Aalami <aalami@gmail.com>
2023-03-19 21:43:08 -0600

    Initial updates to onboarding (#1)

--- TemplateApplicationModules/Sources/TemplateContacts/Contacts.swift
+++ TemplateApplicationModules/Sources/TemplateContacts/Contacts.swift
@@ -16,31 +16,31 @@ public struct Contacts: View {
     let contacts = [
         Contact(
             name: PersonNameComponents(
-                givenName: "Leland",
-                familyName: "Stanford"
+                givenName: "Alexa",
+                familyName: "Aalami"
             ),
             image: Image(systemName: "figure.wave.circle"),
-            title: "University Founder",
-            description: String(localized: "LELAND_STANFORD_BIO", bundle: .module),
-            organization: "Stanford University",
+            title: "Undergrad",
+            description: String(localized: "ALEXA_AALAMI_BIO", bundle: .module),
+            organization: "Columbia Universityaa",
             address: {
                 let address = CNMutablePostalAddress()
                 address.country = "USA"
-                address.state = "CA"
-                address.postalCode = "94305"
-                address.city = "Stanford"
-                address.street = "450 Serra Mall"
+                address.state = "NY"
+                address.postalCode = "10027"
+                address.city = "New York"
+                address.street = "665 W 130th St"
                 return address
             }(),
             contactOptions: [
-                .call("+1 (650) 723-2300"),
-                .text("+1 (650) 723-2300"),
-                .email(addresses: ["contact@stanford.edu"]),
+                .call("+1 (650) 315-3241"),
+                .text("+1 (650) 315-3241"),
+                .email(addresses: ["ara2190@barnard.edu"]),
                 ContactOption(
                     image: Image(systemName: "safari.fill"),
                     title: "Website",
                     action: {
-                        if let url = URL(string: "https://stanford.edu") {
+                        if let url = URL(string: "https://www.sipa.columbia.edu/global-research-impact/initiatives/sipa-public-policy-challenge-grant") {
                            UIApplication.shared.open(url)
                         }
                     }
