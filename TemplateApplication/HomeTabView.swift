fd7a4fd1dd1f920221f770fc04d04ff0f7bf434f
Oliver Aalami <aalami@gmail.com>
2023-03-21 22:45:08 -0700

    added logo to screens and worked on buttons

--- TemplateApplication/HomeTabView.swift
+++ TemplateApplication/HomeTabView.swift
@@ -14,78 +14,97 @@ import TemplateSharedContext
 
 struct HomeTabView: View {
     @State private var userName: String = ""
-
+    
     var body: some View {
-        VStack {
-            Image("OwnYourData_icon_w")
-                .resizable()
-                .scaledToFit()
-                .padding()
-
-            Text("Welcome,")
-                .offset(x: 0, y: -200)
-                .font(Font.system(size: 60))
-                .foregroundColor(Color.accentColor)
-                .fontWeight(.semibold)
-                .padding(.bottom, 1)
-
-            Text("\(userName).")
-                .offset(x: 0, y: -200)
-                .font(Font.system(size: 60))
-                .foregroundColor(Color.accentColor)
-                .fontWeight(.medium)
-
-            Spacer().frame(height: 40)
-
+        GeometryReader { geometry in
             VStack {
-                NavigationLink(destination: HealthRecordView()) {
-                    Text("Records")
-                        .font(.headline)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .foregroundColor(Color.white)
-                        .background(Color(UIColor(named: "ButtonColor_light") ?? .gray))
-                        .cornerRadius(10)
-                        .padding(.leading)
-                        .padding(.trailing)
+                VStack {
+                    Image("OwnYourData_Icon_White-1024")
+                        .resizable()
+                        .foregroundColor(Color.accentColor)
+                        .scaledToFit()
+                        .frame(width: 240, height: 240) // Make it twice as large
+                        .accessibility(label: Text("profile image"))
                 }
-                .padding(.bottom, 20)
-
-                NavigationLink(destination: ClinicalTrialsView()) {
-                    Text("Find Clinical Trials")
-                        .font(.headline)
+                .frame(width: geometry.size.width, height: geometry.size.height / 2, alignment: .top)
+                Spacer().frame(height: 0)
+                
+                //    var body: some View {
+                VStack {
+                    Image("OwnYourData_icon_w")
+                        .resizable()
+                        .scaledToFit()
                         .padding()
-                        .frame(maxWidth: .infinity)
-                        .foregroundColor(.white)
-                        .background(Color(UIColor(named: "ButtonColor_light") ?? .gray))
-                        .cornerRadius(10)
-                        .padding(.leading)
-                        .padding(.trailing)
+                    
+                    Text("Welcome,")
+                        .offset(x: 0, y: -200)
+                        .font(Font.system(size: 60))
+                        .foregroundColor(Color.accentColor)
+                        .fontWeight(.semibold)
+                        .padding(.bottom, 1)
+                    
+                    Text("\(userName).")
+                        .offset(x: 0, y: -200)
+                        .font(Font.system(size: 60))
+                        .foregroundColor(Color.accentColor)
+                        .fontWeight(.medium)
+                    
+                    Spacer().frame(height: 0)
+                    
+                    VStack {
+                        NavigationLink(destination: HealthRecordView()) {
+                            Text("Records")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .foregroundColor(Color.white)
+                                .background(Color(UIColor(named: "ButtonColor_light") ?? .gray))
+                                .cornerRadius(10)
+                                .padding(.leading)
+                                .padding(.trailing)
+                        }
+                        .padding(.bottom, 20)
+                        
+                        NavigationLink(destination: ClinicalTrialsView()) {
+                            Text("Find Clinical Trials")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .foregroundColor(.white)
+                                .background(Color(UIColor(named: "ButtonColor_light") ?? .gray))
+                                .cornerRadius(10)
+                                .padding(.leading)
+                                .padding(.trailing)
+                        }
+                    }
+                                     .padding(.top, -50) // Added negative padding to move the VStack closer
+                                 }
+                .padding(.bottom, 100)
+                .onAppear {
+                    getUserName()
                 }
             }
         }
-        .padding(.bottom, 100)
-        .onAppear {
-            getUserName()
-        }
     }
-
-    func getUserName() {
-        if let user = Auth.auth().currentUser {
-            user.reload { error in
-                if let error = error {
-                    print("Error reloading user: \(error.localizedDescription)")
-                } else {
-                    userName = user.displayName?.split(separator: " ").first.map(String.init) ?? "User"
+            
+            func getUserName() {
+                if let user = Auth.auth().currentUser {
+                    user.reload { error in
+                        if let error = error {
+                            print("Error reloading user: \(error.localizedDescription)")
+                        } else {
+                            userName = user.displayName?.split(separator: " ").first.map(String.init) ?? "User"
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
-}
-
+        
+        struct HomeView_Previews: PreviewProvider {
+            static var previews: some View {
+                HomeView()
+            }
+        }
+        
--- TemplateApplication/ShareView.swift
+++ TemplateApplication/ShareView.swift
@@ -7,15 +7,83 @@
 //
 
 import SwiftUI
+import FHIR
+import HealthKitDataSource
+import TemplateSharedContext
+import HealthKitToFHIRAdapter
+import CardinalKit
+import UIKit
+
 
 struct ShareView: View {
+    @Environment(\.openURL) var openURL
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        GeometryReader { geometry in
+            VStack {
+                VStack {
+                    Image("OwnYourData_Icon_White-1024")
+                        .resizable()
+                        .foregroundColor(Color.accentColor)
+                        .scaledToFit()
+                        .frame(width: 240, height: 240) // Make it twice as large
+                        .accessibility(label: Text("profile image"))
+                }
+                .frame(width: geometry.size.width, height: geometry.size.height / 2.5, alignment: .top)
+                Spacer().frame(height: 0)
+                
+                VStack(spacing: 10) {
+                    VStack {
+                        Text("Share \nHealth Records")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.accentColor)
+                            .multilineTextAlignment(.center)
+                        Button(action: {
+                            let url = URL(string: "x-apple-health://")!
+                            openURL(url)
+                        }) {
+                            Text("Open Health App")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .foregroundColor(Color.white)
+                                .background(Color(UIColor(named: "ButtonColor_light") ?? .gray))
+                                .cornerRadius(10)
+                                .padding(.leading)
+                                .padding(.trailing)
+                        }
+                    }
+                    
+                    Spacer().frame(height: 40)
+                    
+                            VStack(spacing: 10) {
+                                VStack {
+                                    Text("Share \nDocuments")
+                                        .font(.largeTitle)
+                                        .foregroundColor(Color.accentColor)
+                                        .multilineTextAlignment(.center)
+                                    Button(action: {
+                                        let url = URL(string: "x-apple-health://")!
+                                        openURL(url)
+                                    }) {
+                                        Text("Select Documents")
+                                            .font(.headline)
+                                            .fontWeight(.bold)
+                                            .padding()
+                                            .frame(maxWidth: .infinity)
+                                            .foregroundColor(Color.white)
+                                            .background(Color(UIColor(named: "ButtonColor_light") ?? .gray))
+                                            .cornerRadius(10)
+                                            .padding(.leading)
+                                            .padding(.trailing)
+                                    }
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-struct ShareView_Previews: PreviewProvider {
-    static var previews: some View {
-        ShareView()
-    }
-}
