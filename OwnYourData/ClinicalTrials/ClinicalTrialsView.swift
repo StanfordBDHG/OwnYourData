5dce9608b3189cc9f2f138cca108f0b2655f3093
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-05-06 00:31:24 -0700

    Display Trials

--- OwnYourData/ClinicalTrials/ClinicalTrialsView.swift
+++ OwnYourData/ClinicalTrials/ClinicalTrialsView.swift
@@ -17,79 +17,42 @@ struct ClinicalTrialsView: View {
     
     
     var body: some View {
-        if let url = URL(string: "https://www.cancer.gov/about-cancer/treatment/clinical-trials/search") {
-            WebView(url: url)
-                .task {
-                    trials.append(contentsOf: (try? await loadTrials().data) ?? [])
-                    // await testNCIAPI()
-                }
-                .viewStateAlert(state: $viewState)
-        }
-    }
-    
-    
-    private func testNCIAPI() async {
-        do {
-            let testTrialIds = [
-                "NCI-2019-02616",
-                "NCT03386721",
-                "NCI-2015-00414",
-                "NCI-2016-00376",
-                "NCI-2016-00469",
-                "NCI-2017-00679",
-                "NCI-2018-02229",
-                "NCI-2016-01319",
-                "NCI-2016-01435",
-                "NCI-2017-00223",
-                "NCT03391791",
-                "NCT02369029",
-                "NCT02877862",
-                "NCT02565758",
-                "NCT03110159",
-                "NCT02896907",
-                "NCT02816697",
-                "NCT03092856"
-            ]
-            viewState = .processing
-            for testTrialId in testTrialIds {
-                trials.append(try await loadTrial(withId: testTrialId))
-                try await Task.sleep(for: .seconds(2))
-            }
-            viewState = .idle
-        } catch {
-            viewState = .error(AnyLocalizedError(error: error))
-        }
-    }
-        
-    private func loadTrial(withId trialId: String) async throws -> TrialDetail {
-        OpenAPIClientAPI.customHeaders = ["X-API-KEY": "tkMGxBkgOC4TDCUfjcPdw7eeZsuuZual632WpUnH"]
-        CodableHelper.dateFormatter = NICTrialsAPIDateFormatter()
-        
-        return try await withCheckedThrowingContinuation { continuation in
-            TrialsAPI.getTrialById(id: trialId) { data, error in
-                guard let data else {
-                    if let error {
-                        continuation.resume(throwing: error)
-                    } else {
-                        continuation.resume(throwing: DownloadException.responseFailed)
+        List {
+            if trials.isEmpty {
+                ProgressView()
+            } else {
+                ForEach(trials) { trial in
+                    VStack(alignment: .leading) {
+                        Text(trial.briefTitle ?? "No Title")
+                            .bold()
+                        Text(trial.id ?? "No NCI ID")
+                            .bold()
+                            .foregroundStyle(.secondary)
+                        Divider()
+                        Text(trial.detailDescription ?? "-")
+                            .font(.caption)
                     }
-                    return
                 }
-                
-                continuation.resume(returning: data)
             }
         }
+            .navigationTitle("NCI Trials")
+            .task {
+                trials.append(contentsOf: (try? await loadTrials().data) ?? [])
+            }
+            .viewStateAlert(state: $viewState)
     }
+    
     
     private func loadTrials() async throws -> TrialResponse {
-        OpenAPIClientAPI.customHeaders = ["X-API-KEY": "tkMGxBkgOC4TDCUfjcPdw7eeZsuuZual632WpUnH"]
+        OpenAPIClientAPI.customHeaders = ["X-API-KEY": ""]
         CodableHelper.dateFormatter = NICTrialsAPIDateFormatter()
         
         return try await withCheckedThrowingContinuation { continuation in
             TrialsAPI.searchTrialsByGet(
-                size: 1,
+                size: 50,
                 keyword: "breast",
                 trialStatus: "OPEN",
+                phase: "III",
                 primaryPurpose: "TREATMENT",
                 sitesOrgCoordinatesLat: 37.7749,
                 sitesOrgCoordinatesLon: -122.4194,
